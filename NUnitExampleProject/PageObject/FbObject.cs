1f4022c1ff27a297dc47bb37dbfbb22aed0d27df
Abdullah Ahmad Khan <abkhan1006@gmail.com>
2022-10-30 12:00:02 +0530

    abdullah

---
+++ NUnitExampleProject/PageObject/FbObject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnitExampleProject.Utilities;
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace NUnitExampleProject.PageObject
+{
+    internal class FbObject
+    {
+
+        public FbObject()
+        {
+            PageFactory.InitElements(PropertiesCollections.driver, this);
+        }
+
+        //Page Factory Logic
+        [FindsBy(How = How.Name, Using = "email")]
+        public IWebElement txtemail { get; set; }
+
+        public void TestLogin(string email)
+        {
+            txtemail.EnterText(email);
+        }
+    }
+}
---
+++ NUnitExampleProject/TestFiles/Loginfb.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using NUnitExampleProject.Utilities;
+using NUnitExampleProject.PageObject;
+using SeleniumExtras.PageObjects;
+
+namespace NUnitExampleProject.TestFiles
+{
+    public class Loginfb
+    {
+
+
+        IWebDriver driver;
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void fb()
+        {
+            driver.Navigate().GoToUrl("https://www.facebook.com/");
+            FbObject fb = new FbObject();
+            //FbObject fb = new FbObject(PageFactory);
+            fb.TestLogin("abdullahahmadkhan@yahoo.in");
+
+
+        }  
+        }
+}
